7e8453ffe0901a608dcbc38372e9f3b398562d15
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2022-12-02 13:50:40 +0100

    Add request body validation

--- auth/src/api/signup/Signup.controller.ts
+++ auth/src/api/signup/Signup.controller.ts
@@ -1,15 +1,29 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import * as yup from 'yup';
+
+import validate from '../../middlewares';
 
 import { signupService } from './Signup.service';
+
 const router = Router();
 
-router.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
+const schema = yup.object({
+	firstName: yup.string().min(3, 'First name must be at least 3 character long').required('First name is a required'),
+	lastName: yup.string().required('Last name is a required'),
+	username: yup.string().required('Username is a required'),
+	email: yup.string().email('Please provide a valid email').required('Email is a required'),
+	password: yup.string().required(),
+	confirmPassword: yup.string()
+		.oneOf([yup.ref('password'), null], 'Passwords must match')
+});
+
+router.post('/signup', validate(schema), async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		console.log('request', req.body);
-		const response = await signupService(req.body);
-		console.log('response', response);
+		// console.log('request', req.body);
+		// const response = await signupService(req.body);
+		// console.log('response', response);
 	} catch (error) {
-		console.log('error', error);
+		console.log('error +++', error);
 	}
 });
 
