72208e98258a9f59e5003bb3fd80557a19c91c45
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2022-12-25 03:01:30 +0100

    Typo fix

--- auth/src/api/index.ts
+++ auth/src/api/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
 import login from './login';
-import sendVeificationLink from './send-verification-link';
+import sendVerificationLink from './send-verification-link';
 import signup from './signup';
 
 const routes = Router()
 	.use(signup)
 	.use(login)
-	.use(sendVeificationLink);
+	.use(sendVerificationLink);
 
 export default Router().use('/api/auth', routes);
